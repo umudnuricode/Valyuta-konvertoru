551ce4e0f65d7864aa963eaf74713a44fb73afe7
umudnuricode <umnu10@gmail.com>
2025-04-21 12:13:54 +0400

    third

--- index.js
+++ index.js
@@ -14,3 +14,16 @@ document.querySelectorAll(".main31 button").forEach(button => {
         button.style.backgroundColor = "#833AE0";
     })
 });
+document.querySelectorAll("input").forEach(input => {
+    input.addEventListener("input", function () {
+        this.value = this.value.replace(/[^0-9.]/g, '');
+    })
+});
+document.querySelectorAll("input").forEach(input => {
+    input.addEventListener("keydown", function (e) {
+        if (e.key === ',') {
+            e.preventDefault();
+            this.value += ".";
+        }
+    })
+});
