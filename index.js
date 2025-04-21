4260f3d3d94413a13153c392b5b71b88b4154f27
umudnuricode <umnu10@gmail.com>
2025-04-21 11:41:23 +0400

    second

---
+++ index.js
@@ -0,0 +1,16 @@
+document.querySelectorAll(".main3 button").forEach(button => {
+    button.addEventListener("click", function () {
+        document.querySelectorAll(".main3 button").forEach(button => {
+            button.style.backgroundColor = "white";
+        });
+        button.style.backgroundColor = "#833AE0";
+    })
+});
+document.querySelectorAll(".main31 button").forEach(button => {
+    button.addEventListener("click", function () {
+        document.querySelectorAll(".main31 button").forEach(button => {
+            button.style.backgroundColor = "white";
+        });
+        button.style.backgroundColor = "#833AE0";
+    })
+});
