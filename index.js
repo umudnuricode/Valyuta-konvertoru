bb935e5449b33178f1352c02b5058c3803c4fb6c
umudnuricode <umnu10@gmail.com>
2025-04-22 12:41:54 +0400

    fourth

--- index.js
+++ index.js
@@ -27,3 +27,79 @@ document.querySelectorAll("input").forEach(input => {
         }
     })
 });
+// fetch(`https://v6.exchangerate-api.com/v6/30dddd6d6a6f4706f1830f49/pair/USD/RUB`)
+//     .then(res => res.json())
+//     .then(data => {
+//         console.log(data.conversion_rate);
+//     });
+let a = "RUB";
+document.querySelectorAll(".main3 button").forEach(button => {
+    button.addEventListener("click", () => {
+        a = button.innerHTML;
+        // console.log(a);
+        // document.querySelector(".p1").innerHTML=`1 ${a} =`
+        updateRate1();
+        updateRate2();
+        updateRate3();
+        updateInput();
+    })
+});
+let b = "USD";
+document.querySelectorAll(".main31 button").forEach(button => {
+    button.addEventListener("click", () => {
+        b = button.innerHTML;
+        // console.log(b);
+        // document.querySelector(".p2").innerHTML=`1 ${b} =`
+        updateRate1();
+        updateRate2();
+        updateRate3();
+        updateInput();
+    })
+});
+function updateRate1() {
+    fetch(`https://v6.exchangerate-api.com/v6/30dddd6d6a6f4706f1830f49/pair/${a}/${b}`)
+        .then(res => res.json())
+        .then(data => {
+            if (a !== b) {
+                document.querySelector(".p1").innerHTML = `1 ${a} = ${data.conversion_rate} ${b}`;
+            }
+            // else{
+            //     document.querySelector(".p1").innerHTML = `1 ${a} = 1 ${a}`;
+            // }
+            // console.log(data.conversion_rate);
+        });
+};
+function updateRate2() {
+    fetch(`https://v6.exchangerate-api.com/v6/30dddd6d6a6f4706f1830f49/pair/${b}/${a}`)
+        .then(res => res.json())
+        .then(data => {
+            if (a !== b) {
+                document.querySelector(".p2").innerHTML = `1 ${b} = ${data.conversion_rate} ${a}`;
+            }
+            // else{
+            //     document.querySelector(".p2").innerHTML = `1 ${b} = 1 ${b}`;
+            // }
+            // console.log(data.conversion_rate);
+        });
+};
+function updateRate3() {
+    if (a === b) {
+        document.querySelector(".p1").innerHTML = `1 ${a} = 1 ${a}`;
+        document.querySelector(".p2").innerHTML = `1 ${a} = 1 ${a}`;
+    }
+}
+function updateInput() {
+    document.querySelector("#input1").addEventListener("keydown",()=>{
+        fetch(`https://v6.exchangerate-api.com/v6/30dddd6d6a6f4706f1830f49/pair/${a}/${b}`)
+        .then(res => res.json())
+        .then(data=>{
+            if (a === b) {
+                document.querySelector(".p1").innerHTML = `1 ${a} = 1 ${a}`;
+                document.querySelector(".p2").innerHTML = `1 ${a} = 1 ${a}`;
+                document.querySelector("#input2").value=document.querySelector("#input1");
+            }
+            document.querySelector("#input2").value=document.querySelector("#input1").value*data.conversion_rate;
+        })
+    })
+}
+updateInput();
